3519554809410a7265d5ae9f0f6be9599b667211
Ghost_chu <2908803755@qq.com>
2021-05-24 18:39:26 +0800

    given hunters speed buff if they run too far

--- src/main/java/net/mcxk/minehunt/watcher/PlayerMoveWatcher.java
+++ src/main/java/net/mcxk/minehunt/watcher/PlayerMoveWatcher.java
@@ -4,6 +4,9 @@ import net.mcxk.minehunt.MineHunt;
 import net.mcxk.minehunt.game.PlayerRole;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.Optional;
@@ -37,5 +40,24 @@ public class PlayerMoveWatcher {
                 });
             }
         }.runTaskTimer(MineHunt.getInstance(), 0, 80);
+
+        new BukkitRunnable(){
+            @Override
+            public void run() {
+                double farDistance = 0;
+                for(Player runner : MineHunt.getInstance().getGame().getPlayersAsRole(PlayerRole.RUNNER)){
+                    for(Player hunter : MineHunt.getInstance().getGame().getPlayersAsRole(PlayerRole.HUNTER)){
+                        double d = runner.getLocation().distance(hunter.getLocation());
+                        if(d > farDistance)
+                            farDistance = d;
+                    }
+                }
+                if(((int)farDistance % 1000) > 0){
+                    double finalFarDistance = farDistance;
+                    MineHunt.getInstance().getGame().getPlayersAsRole(PlayerRole.HUNTER).forEach(player->player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED,200,((int) finalFarDistance % 1000))));
+                }
+            }
+        }.runTaskTimer(MineHunt.getInstance(),0,200);
+
     }
 }
